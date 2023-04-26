b68336984098fe093a4dee2243751d4c6feee71f
Timjini <hatim.jini@gmail.com>
2023-04-26 18:38:21 +0300

    initial

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,10 @@
+class HomeController < ApplicationController
+
+    def contact 
+        @pagename = "Contact"
+    end
+
+    def about 
+        @pagename = "About"
+    end
+end
---
+++ app/javascript/packs/application.js
@@ -0,0 +1,7 @@
+// app/javascript/packs/application.js
+
+import { hello } from './greeting.js';
+
+document.addEventListener('DOMContentLoaded', () => {
+  console.log(hello());
+});
