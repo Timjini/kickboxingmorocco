1d0ccbbf6e7a576caea68df282fdd56d68a58cb0
Timjini <hatim.jini@gmail.com>
2023-04-28 14:24:55 +0300

    update

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema[7.0].define(version: 2023_04_28_104413) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_28_111434) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -19,7 +18,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_28_104413) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "title"
-
   end
 
 end
