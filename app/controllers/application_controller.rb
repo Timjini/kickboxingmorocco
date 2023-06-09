07c720df7f9bf38e59774675c22282c7946bb301
Timjini <hatim.jini@gmail.com>
2023-06-09 13:32:44 +0300

    change cros and application.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,4 @@
 class ApplicationController < ActionController::Base
+    # skip CSRF token validation for JSON, JavaScript and XML requests
+    protect_from_forgery with: :null_session
 end
--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -8,6 +8,6 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
 
     allow do
       origins 'https://www.coachissam.com'
-      resource '*', headers: :any, methods: [:get, :post, :patch, :put]
+      resource '/api/v1/individual_sessions', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options]
     end
   end
