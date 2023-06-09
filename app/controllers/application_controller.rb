df8532c68379ff5a6aadfbcf57effbc6228eb1ed
Timjini <hatim.jini@gmail.com>
2023-06-09 16:29:56 +0300

    skip token

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 class ApplicationController < ActionController::Base
     # skip CSRF token validation for JSON, JavaScript and XML requests
-    protect_from_forgery with: :exception
+    protect_from_forgery with: :null_session
+    skip_before_action :verify_authenticity_token, if: :json_request?
+    
 end
