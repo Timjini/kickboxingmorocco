b46d14bc8da55e5199e73e964185ed6018d782a7
Timjini <hatim.jini@gmail.com>
2023-06-09 16:32:39 +0300

    json request

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,5 +2,11 @@ class ApplicationController < ActionController::Base
     # skip CSRF token validation for JSON, JavaScript and XML requests
     protect_from_forgery with: :null_session
     skip_before_action :verify_authenticity_token, if: :json_request?
-    
+
+
+    private
+
+    def json_request?
+      request.format.json?
+    end
 end
