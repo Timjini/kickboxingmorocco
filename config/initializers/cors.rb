ff8679fc12a03c480219ca5266533c09561eb53f
Timjini <hatim.jini@gmail.com>
2023-06-09 16:39:04 +0300

    cors update

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,13 +1,9 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   
   allow do
-    origins 'http://localhost:5500' # Replace with your frontend's base URL
-    resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options]
-  end
-
-  allow do
-    origins 'https://www.coachissam.com'
+    origins 'http://localhost:5000', 'https://www.coachissam.com', 'https://kickboxingmorocco.club'
     resource '/api/v1/individual_sessions', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options]
   end
+  
 
 end
