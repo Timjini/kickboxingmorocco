0460ea234fe13be814ba95ffaddb4db9f763386b
Timjini <hatim.jini@gmail.com>
2023-06-09 16:08:42 +0300

    update cookies and cros

--- config/application.rb
+++ config/application.rb
@@ -21,5 +21,6 @@ module Kickboxing
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.middleware.use ActionDispatch::Session::CookieStore
   end
 end
--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,18 +1,13 @@
-# config/initializers/cors.rb
-
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
-    allow do
-      origins '*'
-      resource '*', headers: :any, methods: [:get, :post, :patch, :put]
-    end
-
-    allow do
-      origins 'https://www.coachissam.com'
-      resource '/api/v1/individual_sessions', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options]
-    end
-
-    allow do 
-      origins 'https://coachissam.com/'
-      resource '/api/v1/individual_sessions', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options]
-    end
+  
+  allow do
+    origins 'http://localhost:5000' # Replace with your frontend's base URL
+    resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options]
   end
+
+  allow do
+    origins 'https://www.coachissam.com'
+    resource '/api/v1/individual_sessions', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options]
+  end
+
+end
