a78cf95d4bf6840ae0043fd61daac54ea2142069
Timjini <hatim.jini@gmail.com>
2023-06-09 13:48:55 +0300

    cors update

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -10,4 +10,9 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
       origins 'https://www.coachissam.com'
       resource '/api/v1/individual_sessions', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options]
     end
+
+    allow do 
+      origins 'https://coachissam.com/'
+      resource '/api/v1/individual_sessions', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options]
+    end
   end
