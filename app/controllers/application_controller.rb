4a122117ce5c1f6dd6b6d3370ea81f90b4da0084
Timjini <hatim.jini@gmail.com>
2023-06-09 15:55:15 +0300

    change serializer

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,4 @@
 class ApplicationController < ActionController::Base
     # skip CSRF token validation for JSON, JavaScript and XML requests
-    protect_from_forgery with: :null_session
+    protect_from_forgery with: :exception
 end
