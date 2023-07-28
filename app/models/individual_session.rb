62ff058a139bc1508990042653763d1693c01eba
Timjini <hatim.jini@gmail.com>
2023-07-29 01:11:06 +0300

    return

--- app/models/individual_session.rb
+++ app/models/individual_session.rb
@@ -1,8 +1,8 @@
 # app/models/session_booking.rb
 class IndividualSession < ApplicationRecord
     before_validation :extract_time_from_session_time
-    belongs_to :user , destroy: true
-    belongs_to :coach , destroy: true
+    belongs_to :user
+    belongs_to :coach
 
     private
 
@@ -11,14 +11,5 @@ class IndividualSession < ApplicationRecord
             self.session_time = session_time.strftime("%H:%M")
         end
     end
-
-    def status 
-        if session_date.present? && session_date < Date.today
-            self.status = 0
-        else
-            self.status = 1
-        end
-    end
-
 end
   
